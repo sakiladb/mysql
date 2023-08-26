b91a1a8d14126863fc039d4f2c3f72424f66dfe4
neilotoole <neilotoole@apache.org>
2023-08-26 16:49:58 -0600

    More priviliges

--- 3-sakila-complete.sql
+++ 3-sakila-complete.sql
@@ -1,4 +1,5 @@
 GRANT ALL PRIVILEGES ON *.* TO 'sakila'@'%' WITH GRANT OPTION;
+GRANT CREATE ON *.* TO 'sakila'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
 SELECT 'sakiladb/mysql has successfully initialized.' AS sakiladb_completion_message;
